ebbeb174ac2991fcbe4669538832ff8b643e8f2f
Josh <jspringxc@gmail.com>
2017-03-03 20:20:25 -0500

    add completed algorithm exercises

---
+++ week1/reverse_a_string.rb
@@ -0,0 +1,29 @@
+# Complete the method called reverse_a_string that accepts a string as a parameter and 
+# returns the reverse. The one caveat: Don't use the reverse method that already
+# comes with Ruby!
+
+def reverse_a_string(string)
+  new_string = []
+  string_array = string.split("")
+  string_array.each do |letter|
+    new_string.unshift(letter)
+  end
+  return new_string.join
+
+end
+
+# Driver code - don't touch anything below this line.
+puts "TESTING reverse_a_string..."
+puts
+
+result = reverse_a_string("abcde")
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == "edcba"
+  puts "PASS!"
+else
+  puts "F"
+end
